869d609e28b07b8282e74be7b355af2304bbdac7
Roland Lariotte <reel.errors.0z@icloud.com>
2024-10-27 17:16:28 +0100

    Create endpoint to get list of cats images

--- CatLover/Objects/Enums/ServerEndpoint.swift
+++ CatLover/Objects/Enums/ServerEndpoint.swift
@@ -6,11 +6,15 @@ import Foundation
 
 enum ServerEndpoint {
 
+  private static let baseURL: String = "https://api.thecatapi.com/v1"
+
+  case breedImages(page: Int)
   case breedsList
 
   var url: URL? {
     switch self {
-    case .breedsList: URL(string: "https://api.thecatapi.com/v1/breeds")
+    case .breedImages(let page): URL(string: "\(ServerEndpoint.baseURL)/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=ASC&page=\(page)&limit=15")
+    case .breedsList: URL(string: "\(ServerEndpoint.baseURL)/breeds")
     }
   }
 }
--- CatLoverTests/Cases/Objects/Enums/ServerEndpointTests.swift
+++ CatLoverTests/Cases/Objects/Enums/ServerEndpointTests.swift
@@ -8,6 +8,14 @@ import Testing
 
 struct ServerEndpointTests {
 
+  @Test func urlEndpointToGetBreedImages() throws {
+    let expected = URL(string: "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=ASC&page=1&limit=15")
+
+    let result = ServerEndpoint.breedImages(page: 1).url
+
+    #expect(result == expected)
+  }
+
   @Test func urlEndpointToGetCatBreedsList() throws {
     let expected = URL(string: "https://api.thecatapi.com/v1/breeds")
 

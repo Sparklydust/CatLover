8ab94a0635760e9cf42c8bbac2ef9b8c2458b58e
Roland Lariotte <reel.errors.0z@icloud.com>
2024-10-28 10:20:20 +0100

    Create the BreedModel object for views

---
+++ CatLover/Objects/Models/BreedModel.swift
@@ -0,0 +1,26 @@
+//
+// Copyright © 2024 and confidential to CatLover. All rights reserved.
+//
+
+import Foundation
+
+struct BreedModel {
+
+  let id: String
+  let name: String
+  let origin: String
+  let temperament: String
+  let description: String
+  let lifeSpan: String
+  let imageURL: String?
+
+  init(with data: BreedData) {
+    id = data.id
+    name = data.name
+    origin = data.origin
+    temperament = data.temperament
+    description = data.description
+    lifeSpan = data.lifeSpan
+    imageURL = data.image?.url
+  }
+}

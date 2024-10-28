64db2ad7ba43516b895eee56c7222b6b00419a60
Roland Lariotte <reel.errors.0z@icloud.com>
2024-10-28 09:19:20 +0100

    Create the BreedData object and associated tools for tests

---
+++ CatLover/Doubles/Objects/Data/BreedData+Fakes.swift
@@ -0,0 +1,43 @@
+//
+// Copyright © 2024 and confidential to CatLover. All rights reserved.
+//
+
+import Foundation
+
+extension BreedData {
+
+  /// Create fake ``BreedData`` object for unit tests.
+  /// Returns: Initialized object with fake data.
+  static func fake(
+    id: String = "abys",
+    name: String = "Abyssinian",
+    origin: String = "Egypt",
+    temperament: String = "Active, Energetic, Independent, Intelligent, Gentle",
+    description: String = "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+    lifeSpan: String = "14 - 15",
+    image: BreedData.ImageData = .fake()
+  ) -> BreedData {
+    .init(
+      id: id,
+      name: name,
+      origin: origin,
+      temperament: temperament,
+      description: description,
+      lifeSpan: lifeSpan,
+      image: image
+    )
+  }
+}
+
+extension BreedData.ImageData {
+
+  /// Create fake ``BreedData.ImageData`` object for unit tests.
+  /// Returns: Initialized object with fake data.
+  static func fake(
+    url: String = "breedFake01"
+  ) -> BreedData.ImageData {
+    .init(
+      url: url
+    )
+  }
+}
---
+++ CatLoverTests/Cases/Objects/Data/BreedDataTests.swift
@@ -0,0 +1,20 @@
+//
+// Copyright © 2024 and confidential to CatLover. All rights reserved.
+//
+
+import Foundation
+import Testing
+@testable import CatLover
+
+struct BreedDataTests {
+
+  @Test func requestBreedData_jsonResponseIsSuccessfullyDecoded() async throws {
+    let expected: BreedData = .fake()
+    let data = try ResourceLoader.jsonFake(.breedData)
+
+    let result = try JSONDecoder().decode(BreedData.self, from: data)
+
+    #expect(result == expected, "Decoded `BreedData` must be equal to the `BreedData.json` file content."
+    )
+  }
+}

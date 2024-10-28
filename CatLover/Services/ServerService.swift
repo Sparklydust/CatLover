e91c93aa1c4cbbb9a2cecb5f0497d320f5c7fc65
Roland Lariotte <reel.errors.0z@icloud.com>
2024-10-28 12:46:41 +0100

    Create the ServerService

---
+++ CatLover/Services/ServerService.swift
@@ -0,0 +1,9 @@
+//
+// Copyright © 2024 and confidential to CatLover. All rights reserved.
+//
+
+import Foundation
+
+final actor ServerService {
+  // Intentionally empty
+}
---
+++ CatLoverTests/Cases/Services/ServerServiceTests.swift
@@ -0,0 +1,17 @@
+//
+// Copyright © 2024 and confidential to CatLover. All rights reserved.
+//
+
+import Testing
+@testable import CatLover
+
+final class ServerServiceTests {
+
+  @Test func requestIsSuccessful_requestedDataIsReturned() async throws {
+    #expect(false == true, "Test not yet implemented")
+  }
+
+  @Test func requestIsNoSuccessful_serverErrorIsReturned() async throws {
+    #expect(false == true, "Test not yet implemented")
+  }
+}

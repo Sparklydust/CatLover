9873e4f2d4d17d385ff2a9f06c37a1fb304bbc32
Roland Lariotte <reel.errors.0z@icloud.com>
2024-10-29 14:00:08 +0100

    Instantiate ModelContainer with a custom shared instance

--- CatLover/Objects/Entities/BreedEntity.swift
+++ CatLover/Objects/Entities/BreedEntity.swift
@@ -13,3 +13,20 @@ import SwiftData
     self.breedID = data.id
   }
 }
+
+// MARK: - Model Context
+extension BreedEntity {
+
+  /// Provides the shared context for the ``BreedEntity`` model.
+  ///
+  /// This static property creates a ``ModelContainer`` for the ``BreedEntity`` type and
+  /// retrieves its `mainContext`. The context is used for managing and interacting with instances of
+  /// ``BreedEntity``.
+  /// - Note: This property is isolated to the main actor, ensuring that all interactions with the
+  /// `mainContext` occur on the main thread, maintaining thread safety.
+  /// - Warning: `internalFakeContext` is for Unit Tests only. Do not use in production code.
+  @MainActor static var modelContext: ModelContext {
+    guard internalFakeContext == .none else { return internalFakeContext! }
+    return ModelContainer.shared.mainContext
+  }
+}
---
+++ CatLover/Objects/Entities/ModelContainer+Shared.swift
@@ -0,0 +1,18 @@
+//
+// Copyright © 2024 and confidential to CatLover. All rights reserved.
+//
+
+import SwiftData
+
+extension ModelContainer {
+
+  /// A shared instance of ``ModelContainer`` that is used to manage the app's SwiftData entities.
+  /// - Warning: Must be initialized within a `.modelContainer(_:)` in the ``PumpKitApp``.
+  static let shared: ModelContainer = {
+    do {
+      return try ModelContainer(for: BreedEntity.self)
+    } catch {
+      fatalError("Failed to create `ModelContainer` for the entities: \(error)")
+    }
+  }()
+}

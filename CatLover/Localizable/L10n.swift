bd7c839139a5000e998441d12e395b042a3cc936
Roland Lariotte <reel.errors.0z@icloud.com>
2024-10-30 17:41:40 +0100

    Create breed details view translations keys

--- CatLover/Localizable/L10n.swift
+++ CatLover/Localizable/L10n.swift
@@ -6,6 +6,26 @@ import Foundation
 
 enum L10n {
 
+  public static let breedDetailsGallery = String(
+    localized: "breedDetailsGallery",
+    comment: "Title populate on top of a breed image gallery."
+  )
+
+  public static let breedDetailsLifespan = String(
+    localized: "breedDetailsLifespan",
+    comment: "The title populated next to a breed lifespan information."
+  )
+
+  public static let breedDetailsOrigin = String(
+    localized: "breedDetailsOrigin",
+    comment: "The title populated next to a breed origin information."
+  )
+
+  public static let breedDetailsTemperament = String(
+    localized: "breedDetailsTemperament",
+    comment: "The title populated next to a breed temperament information."
+  )
+
   public static let breedsListNavBarTitle = String(
     localized: "breedsListNavBarTitle",
     comment: "The navigation bar title to populate cat breeds."

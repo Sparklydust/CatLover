7b2619333884419379ff1884ddfbce725f27f775
Roland Lariotte <reel.errors.0z@icloud.com>
2024-10-30 19:20:54 +0100

    Add the gallery title in BreedDetailsView

--- CatLover/Features/BreedDetails/BreedDetailsView.swift
+++ CatLover/Features/BreedDetails/BreedDetailsView.swift
@@ -7,11 +7,15 @@ import SwiftUI
 struct BreedDetailsView: View {
 
   let breed: BreedModel
-  @State private var breedImages: [String] = [] // Holds URLs for the breed images
 
   var body: some View {
     ScrollView {
       BreedCard(size: .large, breed: breed)
+
+      Text(L10n.breedDetailsGallery)
+        .font(.title2)
+        .foregroundStyle(.primary)
+        .fontWeight(.medium)
     }
     .background(Color(.systemGroupedBackground).edgesIgnoringSafeArea(.all))
   }
